cfe290df98b8a9d9835c4a0ae16433aa2b937169
Adarsh-MR <adarshmr1998@gmail.com>
2018-12-08 15:26:45 +0000

    rimo02a: update perf stack from Mi A2 Lite (Daisy)
    * has latest CAF Boostframework API changes such as perfGetFeedback
    
    * from: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- device.mk
+++ device.mk
@@ -300,6 +300,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Updates
 
+# Perf
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
