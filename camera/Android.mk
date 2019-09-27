e52aae526e32ba92dad74f0e9bcd0f8aa82906a7
prashantt41 <prashant.hbtu@gmail.com>
2019-09-27 12:18:58 +0000

    [TEMP]rimo02a: camera: nuke everything to build, address it later
    Signed-off-by: prashantt41 <prashant.hbtu@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,23 +1,34 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8909
-MM_V4L2_DRIVER_LIST += msm8952
-MM_V4L2_DRIVER_LIST += msm8996
-MM_V4L2_DRIVER_LIST += msm8992
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
-  endif
-endif
+LOCAL_C_INCLUDES := \
+    system/media/camera/include \
+    frameworks/native/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libbase \
+    libsensor \
+    libhidltransport \
+    libnativewindow \
+    libgui \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
